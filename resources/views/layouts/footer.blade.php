ce9de0af3b995d460d957adabe4dcb448b3061ea
takeuchi <takeuchi@10.85.17.181>
2024-01-25 14:03:23 +0900

    [add]Bladeテンプレートファイルの追加
    ref 継承元となるテンプレートファイルを追加しました。各ファイルはview>layoutsに格納しています。CSSはレスポンシブを考慮してtailwindcssをCDNで埋め込んでいます。

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+@section('footer')
+	<footer class="footer">
+		フッター部分です
+	</footer>
+@endsection
---
+++ resources/views/nutritions/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+@section('keywords', 'Nutrition Calc')
+@section('description', 'This page is Home.')
+
+@include('layouts.header')
+
+@section('content')
+    <h1 class="text-3xl font-bold underline">
+    	Hello world!
+    </h1>
+@endsection
+
+@include('layouts.footer')
