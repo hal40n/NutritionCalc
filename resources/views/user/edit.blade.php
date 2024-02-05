6e0d062d4661611f5902fb4681596cb35d8c11f4
takeuchi <takeuchi@192.168.1.210>
2024-02-05 16:16:35 +0900

    [update] ログインユーザーの情報変更フォームを作成しました。

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -1,75 +1,68 @@
 <x-app-layout> <x-slot name='title'> ユーザー情報入力 </x-slot>
 
-<section class="bg-amber-50 pt-20 lg:pt-[120px] pb-12 lg:pb-[90px] relative z-20 overflow-hidden">
+<section class="bg-amber-50 pt-20 pb-12 relative z-20 overflow-hidden">
 	<div class="container">
-		<!-- component -->
-		<div class="flex h-screen bg-gray-200 items-center justify-center  mt-32 mb-32">
-			<div class="grid bg-white rounded-lg shadow-xl w-11/12 md:w-9/12 lg:w-1/2">
-				<div class="flex justify-center py-4">
-					<div class="flex bg-purple-200 rounded-full md:p-4 p-2 border-2 border-purple-300">
-						<svg class="w-8 h-8 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"></path></svg>
-					</div>
-				</div>
-
-				<div class="flex justify-center">
-					<div class="flex">
-						<h1 class="text-gray-600 font-bold md:text-2xl text-xl">Tailwind Form</h1>
-					</div>
-				</div>
-
-				<div class="grid grid-cols-1 mt-5 mx-7">
-					<label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Input 1</label> <input
-						class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" placeholder="Input 1" />
-				</div>
-
-				<div class="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-8 mt-5 mx-7">
-					<div class="grid grid-cols-1">
-						<label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Input 2</label> <input
-							class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" placeholder="Input 2" />
-					</div>
-					<div class="grid grid-cols-1">
-						<label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Input 3</label> <input
-							class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" placeholder="Input 3" />
-					</div>
-				</div>
-
-				<div class="grid grid-cols-1 mt-5 mx-7">
-					<label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Active Level</label> <select
-						class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
-						<option>Low</option>
-						<option>Normal</option>
-						<option>High</option>
-					</select>
-				</div>
-
-				<div class="grid grid-cols-1 mt-5 mx-7">
-					<label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Another Input</label> <input
-						class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" placeholder="Another Input" />
-				</div>
-
-				<div class="grid grid-cols-1 mt-5 mx-7">
-					<label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold mb-1">Upload Photo</label>
-					<div class='flex items-center justify-center w-full'>
-						<label class='flex flex-col border-4 border-dashed w-full h-32 hover:bg-gray-100 hover:border-purple-300 group'>
-							<div class='flex flex-col items-center justify-center pt-7'>
-								<svg class="w-10 h-10 text-purple-400 group-hover:text-purple-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-									<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-										d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
-								<p class='lowercase text-sm text-gray-400 group-hover:text-purple-600 pt-1 tracking-wider'>Select a photo</p>
-							</div>
-							<input type='file' class="hidden" />
-						</label>
-					</div>
-				</div>
-
-				<div class='flex items-center justify-center  md:gap-8 gap-4 pt-5 pb-5'>
-					<button class='w-auto bg-gray-500 hover:bg-gray-700 rounded-lg shadow-xl font-medium text-white px-4 py-2'>Cancel</button>
-					<button class='w-auto bg-purple-500 hover:bg-purple-700 rounded-lg shadow-xl font-medium text-white px-4 py-2'>Create</button>
-				</div>
-
-			</div>
+		<div class="text-center mx-auto">
+			<h2 class="font-bold text-3xl sm:text-4xl md:text-[40px] text-green-700 mb-4">登録情報変更</h2>
+			<p class="font-semibold text-[2vw] md:text-lg text-body-color block">{{ Auth::user()->name }}さんの情報を変更します</p>
 		</div>
+		<form action="{{ route('user.update', ['user' => Auth::user()]) }}" method="POST">
+			@csrf <input type="hidden" name="_method" value="PUT"> <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
+			<div class="flex h-screen items-center justify-center mb-5">
+				<div class="grid bg-white rounded-lg shadow-xl w-3/4 py-4">
+					<div class="grid grid-cols-1 mt-5 mx-7 px-2">
+						<label class="md:text-sm text-xs text-green-700 font-semibold">氏名</label> <input
+							class="py-2 px-3 rounded-lg border-2 border-green-700 mt-2 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-transparent" type="text" name="name" value="{{ Auth::user()->name }}" />
+					</div>
+					<div class="grid grid-cols-1 mt-5 mx-7 px-2">
+						<label class="md:text-sm text-xs text-green-700 font-semibold">メールアドレス</label> <input
+							class="py-2 px-3 rounded-lg border-2 border-green-700 mt-2 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-transparent" type="text" name="email"
+							value="{{ Auth::user()->email }}" />
+					</div>
+					<div class="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-8 mt-5 mx-7 px-2">
+						<div class="grid grid-cols-1">
+							<label class="md:text-sm text-xs text-green-700 font-semibold">年齢</label> <input
+								class="py-2 px-3 rounded-lg border-2 border-green-700 mt-2 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-transparent" type="text" name="age" value="{{ Auth::user()->age }}" />
+						</div>
+						<div class="grid grid-cols-1">
+							<label class="md:text-sm text-xs text-green-700 font-semibold">性別</label> <select
+								class="py-2 px-3 rounded-lg border-2 border-green-700 mt-1 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-transparent" name="sex">
+								<option value="MAN">男</option>
+								<option value="WOMAN">女</option>
+							</select>
+						</div>
+					</div>
+					<div class="grid grid-cols-1 mt-5 mx-7 px-2">
+						<label class="md:text-sm text-xs text-green-700 font-semibold">活動レベル</label> <select
+							class="py-2 px-3 rounded-lg border-2 border-green-700 mt-1 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-transparent" name="activeLevel">
+							<option value="LOW">低い</option>
+							<option value="NORMAL">普通</option>
+							<option value="HIGH">高い</option>
+						</select>
+					</div>
+					<div class="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-8 mt-5 mx-7 px-2">
+						<div class="grid grid-cols-1">
+							<label class="md:text-sm text-xs text-green-700 font-semibold">身長</label> <input
+								class="py-2 px-3 rounded-lg border-2 border-green-700 mt-1 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-transparent" type="text" name="height"
+								value="{{ Auth::user()->height }}" />
+							<p>cm</p>
+						</div>
+						<div class="grid grid-cols-1">
+							<label class="md:text-sm text-xs text-green-700 font-semibold">体重</label> <input
+								class="py-2 px-3 rounded-lg border-2 border-green-700 mt-1 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-transparent" type="text" name="weight"
+								value="{{ Auth::user()->weight }}" />
+							<p>kg</p>
+						</div>
+					</div>
+					<div class='flex items-center justify-center  md:gap-8 gap-4 mt-3 pt-5 pb-5'>
+						<button type="submit" name="updateExec" class='w-auto bg-green-700 hover:bg-green-900 rounded-lg shadow-xl font-medium text-white px-4 py-2'>更新</button>
+						<button class='w-auto bg-gray-500 hover:bg-gray-700 rounded-lg shadow-xl font-medium text-white px-4 py-2'>
+							<a href="{{ route('home') }}">戻る</a>
+						</button>
+					</div>
+				</div>
+			</div>
+		</form>
 	</div>
 </section>
 </x-app-layout>
